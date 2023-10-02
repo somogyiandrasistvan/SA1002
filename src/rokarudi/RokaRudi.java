c8e92b7e24ef1da0e75a3f6443b15899a2cdbbd7
somogyiandrasistvan <somogyiandraska@gmail.com>
2023-10-02 13:39:21 +0200

    feltoltes meg egyszer

--- src/rokarudi/RokaRudi.java
+++ src/rokarudi/RokaRudi.java
@@ -13,11 +13,13 @@ public class RokaRudi {
     private static void feladat() {
         String tomb[] = feltoltes();
         kiiras(tomb);
-        bekeres();
+        int szam = bekeres();
+        kever(tomb, szam);
+        kiiras(tomb);
     }
 
     private static String[] feltoltes() {
-        String tomb[] = new String[24];
+        String tomb[] = new String[22];
         String jelek[] = {"SZ", "M", "T", "L"};
         String szamok[] = {"K", "ÁSZ", "X", "IX", "VIII"};
 
@@ -37,17 +39,48 @@ public class RokaRudi {
                 System.out.println(" ");
             }
         }
+        System.out.println(" ");
     }
 
-    private static void bekeres() {
+    private static int bekeres() {
         boolean jo = true;
+        int szam;
         do {
             System.out.println("Kérek egy számot: [1-3]");
-            int szam = sc.nextInt();
-            if(szam > 1 || szam < 4){
+            szam = sc.nextInt();
+            if (szam > 0 & szam < 4) {
+                jo = true;
+            } else {
                 jo = false;
             }
         } while (jo == false);
+
+        return szam;
+    }
+
+    private static void kever(String tomb[], int szam) {
+        String ujTomb[] = new String[22];
+        if (szam == 1) {
+            for (int i = 1; i < 7; i++) {
+                ujTomb[i] = tomb[20 - (i - 1) * 3];
+            }
+        }
+        if (szam == 2) {
+            for (int i = 7; i < 14; i++) {
+                ujTomb[i] = tomb[19 - (i - 1) * 3];
+            }
+        } else {
+            for (int i = 14; i < 21; i++) {
+                ujTomb[i] = tomb[21 - (i - 1) * 3];
+            }
+        }
+        for (int i = 1; i < tomb.length; i++) {
+            tomb[i] = ujTomb[i];
+        }
+    }
+
+    private static void kiiras(String tomb[]) {
+        System.out.println("A gondolt lap: " + tomb[11]);
     }
 
 }
